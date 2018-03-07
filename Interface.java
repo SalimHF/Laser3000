02145724a026819d2f7be30ae0c0389176cf9f9a
SalimHF <37149502+SalimHF@users.noreply.github.com>
2018-03-07 15:53:04 +0100

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,29 @@
+import javax.swing.	JFrame;
+
+public class Interface extends JFrame { 
+	
+	
+	int tailleX=800, tailleY = 800, tailleMenu=85;
+	
+	
+	//Constructeur
+	public Interface() {
+		this.setTitle("Laser3000");
+        this.setSize(tailleX, tailleY+tailleMenu);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+
+
+
+}
